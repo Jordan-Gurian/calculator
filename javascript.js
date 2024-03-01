be2bcf687b2fdb1a64db48f9f5a0d749f1c2fd44
Jordan Gurian <gurianjordan@gmail.com>
2024-03-01 14:48:15 -0800

    Allow basic arithmetic using calculator
    Some functionality remains to be added, but basic math can be performed

--- javascript.js
+++ javascript.js
@@ -1,16 +1,35 @@
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
+let operatorArray = ['+', '-', 'x', '÷'];
 
 let display = document.querySelector('.display');
-let numberButton = Array.from(document.querySelectorAll('.number'));
+let numberButton = Array.from(document.querySelectorAll('.number, .operator'));
+let equalButton = document.querySelector('#equals')
+
+equalButton.addEventListener('click', function() {
+    setNums(display.textContent);
+    display.textContent = operate(firstNum, secondNum, operator);
+    return
+})
 
 numberButton.forEach(numToDisplay);
+
+function setNums(str) {
+    let operatorInd = str.indexOf(operator);
+    firstNum = Number(str.substring(0, operatorInd));
+    secondNum = Number(str.substring(operatorInd+1, str.length));
+    return firstNum, secondNum
+}
 
 function numToDisplay(item) {
     item.addEventListener('click', function() {
         display.textContent += item.textContent;
+        if (operatorArray.some((operand) => operand === item.textContent)) {
+            operator = item.textContent;
+        }
     });
+    return
 }
 
 function addTwoNums(num1, num2) {
@@ -38,10 +57,10 @@ function operate(num1, num2, operator) {
         case '-':
             res = subtractTwoNums(num1, num2);
             break;
-        case '*':
+        case 'x':
             res = multiplyTwoNums(num1, num2);
             break;
-        case '/':
+        case '÷':
             res = divideTwoNums(num1, num2);
             break;
         default:
